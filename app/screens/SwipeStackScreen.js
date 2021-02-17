3e25da832ad2a715d06c521940186449bde67d45
John <jgilpn@gmail.com>
2021-02-17 00:53:12 -0800

    Merged and added Swipe Screen to Tab Nav

--- app/screens/SwipeStackScreen.js
+++ app/screens/SwipeStackScreen.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { createStackNavigator } from '@react-navigation/stack'
 
-import FavouriteScreen from "./FavouriteScreen";
+import SwipeScreen from './SwipeScreen'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 const SwipeStackScreen = ({ navigation }) => {
   return (
-    <Stack.Navigator initialRouteName="Home" mode="modal">
+    <Stack.Navigator initialRouteName='Home' mode='modal'>
       <Stack.Screen
-        name="Swipe"
-        component={FavouriteScreen}
-        options={{ headerTitleAlign: "center" }}
+        name='Swipe'
+        component={SwipeScreen}
+        options={{ headerTitleAlign: 'center' }}
       />
     </Stack.Navigator>
-  );
-};
+  )
+}
 
-export default SwipeStackScreen;
+export default SwipeStackScreen
