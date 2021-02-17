86086fc86014321a312ddb6590d43e2739a15383
John <jgilpn@gmail.com>
2021-02-16 19:52:53 -0800

    added janky ass card swipe looking mf that doesnt work yet

--- App.js
+++ App.js
@@ -1,24 +1,29 @@
-import React from "react";
-import { Platform, StyleSheet, StatusBar } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import React from 'react'
+import { Platform, StyleSheet, StatusBar } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-import MainScreen from "./app/screens/MainScreen";
+import MainScreen from './app/screens/MainScreen'
+import SwipeScreen from './app/screens/SwipeScreen'
 
-
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer style={styles.container}>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={MainScreen} />
+        <Stack.Navigator initialRouteName='Swipe'>
+          <Stack.Screen name='Home' component={MainScreen} />
+          <Stack.Screen
+            name='Swipe'
+            component={SwipeScreen}
+            options={{ headerShown: false }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -26,6 +31,6 @@ const styles = StyleSheet.create({
     flex: 1,
     // alignItems: "center",
     // justifyContent: "center",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
-});
+})
---
+++ app/screens/SwipeScreen.js
@@ -0,0 +1,124 @@
+import React, { useState, useEffect, useRef } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image,
+  Animated,
+  PanResponder,
+} from 'react-native'
+import { apiKey, restaurantFetch } from '../components/constants/Constants'
+
+const SCREEN_HEIGHT = Dimensions.get('window').height
+const SCREEN_WIDTH = Dimensions.get('window').width
+
+const SwipeScreen = () => {
+  const [location, setLocation] = useState('Vancouver, BC')
+  const [offset, setOffset] = useState(0)
+  const [isLoading, setLoading] = useState(false)
+  const [data, setData] = useState(null)
+
+  const formatData = (rawData) => {
+    let data = rawData.map((restaurant) => ({
+      id: restaurant.id,
+      name: restaurant.name,
+      coordinates: restaurant.coordinates,
+      location: restaurant.location,
+      imageURL: restaurant.image_url,
+      url: restaurant.url,
+      rating: restaurant.rating,
+      price: restaurant.price ? restaurant.price : '',
+      phone: restaurant.phone ? restaurant.phone : '',
+      display_phone: restaurant.display_phone ? restaurant.display_phone : '',
+    }))
+    return data
+  }
+
+  const swipeStack = () =>
+    data &&
+    data.map((restaurant) => {
+      const pan = new Animated.ValueXY()
+      const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: (evt, gestureState) => true,
+        onPanResponderMove: (evt, gestureState) => {
+          pan.setValue({ x: gestureState.dx, y: gestureState.dy })
+        },
+        onPanResponderRelease: (evt, gestureState) => {},
+      })
+
+      return (
+        <Animated.View
+          {...panResponder.panHandlers}
+          key={restaurant.id}
+          style={[
+            { transform: pan.getTranslateTransform() },
+            {
+              height: SCREEN_HEIGHT - 120,
+              width: SCREEN_WIDTH,
+              padding: 10,
+              position: 'absolute',
+            },
+          ]}
+        >
+          {data && (
+            <Image
+              style={{
+                flex: 1,
+                height: null,
+                width: null,
+                resizeMode: 'cover',
+                borderRadius: 20,
+              }}
+              source={{ uri: restaurant.imageURL }}
+            />
+          )}
+        </Animated.View>
+      )
+    })
+
+  useEffect(() => {
+    const abortController = new AbortController()
+    let url = `${restaurantFetch.url}&offset=${offset}&location=${location}`
+
+    let loadData = async () => {
+      try {
+        setLoading(true)
+        const response = await fetch(url, {
+          signal: abortController.signal,
+          method: 'GET',
+          headers: {
+            Authorization: apiKey.bearer,
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((res) => res.json())
+          .catch((err) => {
+            console.log(err)
+          })
+
+        let businessData = formatData(response.businesses)
+        setData(businessData)
+      } catch (e) {
+        console.log(e)
+        setLoading(false)
+      }
+    }
+
+    loadData()
+    return () => {
+      console.log('Roulette Screen Aborting...')
+      abortController.abort()
+    }
+  }, []) // PUT OFFSET HERE WHEN WE WANT IT TO CHANGE ON OFFSET UPDATE
+
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ height: 60 }}></View>
+      <View style={{ flex: 1 }}>{data && swipeStack()}</View>
+      <View style={{ height: 60 }}></View>
+    </View>
+  )
+}
+
+export default SwipeScreen
